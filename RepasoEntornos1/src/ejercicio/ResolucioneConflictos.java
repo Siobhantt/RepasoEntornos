3b54787670a107b523bbd16ab9afeb8d5c0e6de7
Siobhantt <luisa.cardozo@iesnervion.es>
2023-01-25 17:04:16 +0100

    IsaK

---
+++ RepasoEntornos1/src/ejercicio/ResolucioneConflictos.java
@@ -0,0 +1,9 @@
+package ejercicio;
+
+public class ResolucioneConflictos {
+
+	public static void main(String[] args) {
+		System.out.println("Isa pedazo de pelirroja guapa, tas pa mojar pan.");
+	}
+
+}
